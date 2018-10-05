b37ebfff18e0e79d80e47d5c3a404c4256d195b8
Ziye Wang <zwang@LT-C02SF4R4G8WP.local>
2018-10-05 15:28:10 -0400

    update server js

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const http = require('http');
 
-console.log("start the server!");
 console.log("syncing with engage server!");
 http.createServer(function (req, res) {
   res.write('Server is running in docker container at 8081'); 
