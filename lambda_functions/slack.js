032ed32ed88ac674903f4b3a29eb7a4bd2908d42
Ziye Wang <zwang@LT-C02SF4R4G8WP.local>
2018-10-17 10:55:25 -0400

    update cloud formation template

--- lambda_functions/slack.js
+++ lambda_functions/slack.js
@@ -1,37 +1,39 @@
-const url = require('url');
-const https = require('https');
-const util = require('util');
+const url = require("url");
+const https = require("https");
+const util = require("util");
 
 const POST_OPTIONS = {
-    hostname: 'hooks.slack.com',
-    path: 'https://hooks.slack.com/services/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',
-    method: 'POST',
-  };
+  hostname: "hooks.slack.com",
+  path:
+    "https://hooks.slack.com/services/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",
+  method: "POST"
+};
 
 exports.handler = (event, context, callback) => {
-    
-    console.log(JSON.stringify(event));
-    const stage = 'Code Build';
-    const state = event.detail['build-status'];
-    const title = stage +' '+state;
-    
-    const message = {
-        channel: 'general',
-        attachments: [
-        {
-            "color": state.toLowerCase()==='failed'?"#ff6347":"#36a64f",
-            "pretext": state,
-            "author_name": "ziye",
-            "title": title,
-            "title_link": "https://api.slack.com/",
-            "text": state,
-          
-        }
-    ]};
-    const r = https.request(POST_OPTIONS, (res)=> {
-                        res.setEncoding('utf8');
-                        res.on('data',  (data) => context.succeed("Message Sent: " + data));
-    }).on("error", e =>context.fail("Failed: " + e));
-    r.write(util.format("%j", message));
-    r.end();
+  console.log(JSON.stringify(event));
+  const stage = "Code Build";
+  const state = event.detail["build-status"];
+  const title = stage + " " + state;
+
+  const message = {
+    channel: "general",
+    attachments: [
+      {
+        color: state.toLowerCase() === "failed" ? "#ff6347" : "#36a64f",
+        pretext: state,
+        author_name: "ziye",
+        title: title,
+        title_link: "https://api.slack.com/",
+        text: state
+      }
+    ]
+  };
+  const response = https
+    .request(POST_OPTIONS, res => {
+      res.setEncoding("utf8");
+      res.on("data", data => context.succeed("Message Sent: " + data));
+    })
+    .on("error", e => context.fail("Failed: " + e));
+  response.write(util.format("%j", message));
+  response.end();
 };
