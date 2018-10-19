9b1d9dfc433f26148a9262a6b14dbc7bc2cd4536
Ziye Wang <zwang@LT-C02SF4R4G8WP.local>
2018-10-19 13:19:49 -0400

    sync data

--- syncService.js
+++ syncService.js
@@ -1,16 +1,6 @@
 const http = require('http');
 http.createServer(function (req, res) {
   res.write('Node server is listening at 8081...\n');
-  res.write('Node server starting syncing data between CB and Neptune...\n');
-  res.write('Syncing workorders...\n');
-  res.write('Syncing flocs...\n');
-  res.write('Syncing maintance plans...\n');
-  res.write('Syncing work centers...\n');
-  res.write('Syncing compliance date...\n');
-  res.write('Syncing inspectors...\n');
-  res.write('Syncing logs...\n');
-  res.write('Syncing line of businesses...\n');
-  res.write('Syncing divisions...\n');
-  res.write('Syncing divisions...\n');
+  res.write('Node server starting syncing data...\n');
   res.end(); 
 }).listen(8081); 
