c03b85c9d267dde68700576a818a5475dff4bc31
EC2 Default User <ec2-user@ip-172-31-37-137.us-east-2.compute.internal>
2018-10-31 15:00:14 +0000

    add new line

--- syncService.js
+++ syncService.js
@@ -1,6 +1,8 @@
 const http = require('http');
+const ip = require("ip");
 http.createServer(function (req, res) {
   res.write('Node server is listening at 8081...\n');
   res.write('Node server starting syncing data...\n');
+  res.write(ip.address());
   res.end(); 
 }).listen(8081); 
