ff75c88f90ff26d5392f6effde51fa0a952bed3d
ziye wang <ziyewang1986@gmail.com>
2019-07-16 11:56:20 -0400

    test

--- syncService.js
+++ syncService.js
@@ -2,5 +2,6 @@ const http = require('http');
 http.createServer(function (req, res) {
   res.write('Node server is listening at 8081...\n');
   res.write('Node server starting syncing data...\n');
+  res.write('AWS secrets are here.');
   res.end(); 
 }).listen(8081); 
