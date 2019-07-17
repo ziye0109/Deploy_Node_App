f3654d62fd5743cf8cad01584620584485407a87
ziye wang <ziyewang1986@gmail.com>
2019-07-17 16:23:15 -0400

    first commit

--- secret_check/install_git_secret.sh
+++ secret_check/install_git_secret.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 set -e
 
-echo "Check if git-secrets exist"
+echo "Checking if git-secrets installed"
 if [ -x "$(command -v git-secrets)" ]; then 
-    echo "git-secrets already installed." >&2
+    echo "Git Secrets already installed." >&2
     exit 1
 fi
 
@@ -32,17 +32,13 @@ echo "Add AWS as provider and register common patterns for AWS"
 git secrets --register-aws
 
 echo "Add Other prohibited pattern"
-git secrets --add 'ES_AUTH\s*:\s*(.+\s*.*):(.+\s*.*)$'
-git secrets --add 'ES_HOST\s*:\s*(http|https)://(.+\s*.*):(.+\s*.*)@.+'
-git secrets --add 'CLIENT_ID\s*:\s*[\w.]+'
 git secrets --add 'CLIENT_SECRET\s*:\s*[\w.]+'
-git secrets --add '(password|PASSWORD)\s*(:|=)\s*.+'
-git secrets --add '\(username|USERNAME)\s*(:|=)\s*.+\i'
+git secrets --add '/password\s*(:|=)\s*.+/i'
+git secrets --add '/username\s*(:|=)\s*.+/i'
 git secrets --add 'SESSION_KEY\s*(:|=)\s*.+'
-git secrets --add 'AUTH_TOKEN\s*(:|=)\s*.+'
+git secrets --add 'TOKEN\s*(:|=)\s*.+'
 git secrets --add 'IDENTITY_KEY_PASSPHRASE\s*(:|=)\s*.+'
-git secrets --add '\s*TOKEN\s*'
 git secrets --add 'API_KEY'
 
-echo "Installation is done"
+echo "Git Secrets commit hooks is ready!"
 
