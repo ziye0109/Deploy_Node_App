4505e0c94f3766a94517747525142f769e8e64ed
ziye wang <ziyewang1986@gmail.com>
2019-07-17 12:04:03 -0400

    hello

---
+++ secret_check/install_git_secret.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+set -e
+
+echo "Check if git-secrets exist"
+if [ -x "$(command -v git-secrets)" ]; then 
+    echo "git-secrets already installed." >&2
+    exit 1
+fi
+
+while true; do 
+    read -p "Do you want to install git-secrets to preventing commit your secret?" yn
+    case $yn in 
+        [Yy]* ) echo "intall"; break;;
+        [Nn]* ) exit;;
+        *) echo "Please answer yes or no.";;
+    esac
+done
+
+echo "Download git-secret"
+git clone https://github.com/sobolevn/git-secret.git git-secret
+
+echo "Build git-secret"
+cd git-secret && make build
+
+echo "Intall git-secret"
+PREFIX="/usr/local" make install
+
+echo "Intall git-secret hooks"
+git secrets --install -f
+
+echo "Add AWS as provider"
+git secrets --add-provider -- git secrets --aws-provider
+
+echo "Add Other prohibited pattern"
+git secrets --add 'ES_AUTH\s*:\s*(.+\s*.*):(.+\s*.*)$'
+git secrets --add 'ES_HOST\s*:\s*(http|https)://(.+\s*.*):(.+\s*.*)@.+'
+git secrets --add 'CLIENT_ID\s*:\s*[\w.]+'
+git secrets --add 'CLIENT_SECRET\s*:\s*[\w.]+'
+git secrets --add '(password|PASSWORD)\s*(:|=)\s*.+'
+git secrets --add '(username|USERNAME)\s*(:|=)\s*.+'
+git secrets --add 'SESSION_KEY\s*(:|=)\s*.+'
+git secrets --add 'AUTH_TOKEN\s*(:|=)\s*.+'
+git secrets --add 'IDENTITY_KEY_PASSPHRASE\s*(:|=)\s*.+'
+
+echo "Installation is done"
+
--- syncService.js
+++ syncService.js
@@ -1,7 +1,17 @@
 const http = require('http');
+
+const CLIENT_SECRET = '';
+const password = 'Miranda@0109';
+const username = 'ziye';
+const SESSION_KEY = '';
+const AUTH_TOKEN = ''
+const IDENTITY_KEY_PASSPHRASE = '';
+const aws_access_key = '';
+const aws_account_id = '';
+
 http.createServer(function (req, res) {
   res.write('Node server is listening at 8081...\n');
   res.write('Node server starting syncing data...\n');
   res.write('AWS secrets are here.');
-  res.end(); 
+  res.end();
 }).listen(8081); 
